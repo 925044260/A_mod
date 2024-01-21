650b6c2bbf8f903c7cf5dcd99b64cda37fb61b5c
925044260 <925044260@qq.com>
2024-01-22 02:29:43 +0800

    v1

---
+++ A_mod/A_mod.go
@@ -0,0 +1,7 @@
+package A_mod
+
+import B "github.com/925044260/B/v2"
+
+func Go() string {
+	return B.Hello
+}
---
+++ A_mod/go.mod
@@ -0,0 +1,5 @@
+module github.com/925044260/A_mod
+
+go 1.21.6
+
+require github.com/925044260/B/v2 v2.0.2
